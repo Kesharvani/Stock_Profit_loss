714df640cb69ba1e7d42cd758f5aafb15930015e
Shubham Kesharvani <94446874+Kesharvani@users.noreply.github.com>
2022-01-13 19:23:32 +0530

    error handaling

--- stock.js
+++ stock.js
@@ -9,21 +9,30 @@ var outputDivSelector=document.querySelector("#outputdiv");
 
 profitLossButtonSelector.addEventListener("click", function submitHandler()
 {
+    if(initialPriceSelector==="" || initialPriceSelector.value<=0 || quantitySelector.value<=0 ||quantitySelector==="" || currentPriceSelector.value<=0 || currentPriceSelector==="")
+    {
+        outputDivSelector.innerText="Please Enter currect value! Field value should not be negative, blank or Zero";
+    }
+    else{
 
     calculateProfitAndLoss(Number(currentPriceSelector.value),Number(initialPriceSelector.value), Number(quantitySelector.value));
+    }
 });
 
 function calculateProfitAndLoss(currentPriceSelector, initialPriceSelector, quantitySelector)
 {
+    var buyingPrice=initialPriceSelector*quantitySelector;
+    var sellingPrice=currentPriceSelector*quantitySelector;
     if(currentPriceSelector > initialPriceSelector)
     {
-        var buyingPrice=initialPriceSelector*quantitySelector;
-        var sellingPrice=currentPriceSelector*quantitySelector;
 
-        outputDivSelector.innerText="Profit Percentage is: " + ((sellingPrice-buyingPrice)*100)/buyingPrice +"%";
+        outputDivSelector.innerText="Profit Percentage is: " + ((sellingPrice-buyingPrice)*100)/buyingPrice +"%" + " Total profit is: "+(sellingPrice-buyingPrice);
     }
     else{
+
         
-         outputDivSelector.innerText="Loss Percentage is: " + ((buyingPrice-sellingPrice)*100)/buyingPrice +"%";
+
+         outputDivSelector.innerText=
+         "Loss Percentage is: "+ ((buyingPrice-sellingPrice) * 100)/buyingPrice +"%" + " Total loss is: " + (buyingPrice-sellingPrice);
     }
 }
